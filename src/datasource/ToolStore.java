f2090552e92d1e598ab6eba1d1303190865376a5
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2023-07-02 14:42:49 -0500

    Refactor test to account for chargeable day check

--- src/datasource/ToolStore.java
+++ src/datasource/ToolStore.java
@@ -1,43 +1,75 @@
 package datasource;
 
+import static util.DayChecker.DayType.*;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import model.Tool;
 import model.ToolType;
+import util.DayChecker.DayType;
 
 public class ToolStore {
-    private static ToolStore instance;
-    private final List<Tool> tools;
+  private static ToolStore instance;
+  private final List<Tool> tools;
 
-    private ToolStore() {
-        tools = new ArrayList<>();
+  private ToolStore() {
+    tools = new ArrayList<>();
 
-        initDataSource();
+    initDataSource();
+  }
+
+  public static ToolStore getInstance() {
+    if (instance == null) {
+      instance = new ToolStore();
     }
 
-    public static ToolStore getInstance() {
-        if (instance == null) {
-            instance = new ToolStore();
-        }
+    return instance;
+  }
 
-        return instance;
-    }
+  private void initDataSource() {
+    ToolType ladder =
+        new ToolType(
+            "Ladder",
+            1.99,
+            new HashSet<DayType>() {
+              {
+                add(WEEKDAY);
+                add(WEEKEND);
+              }
+            });
 
-    private void initDataSource() {
-        ToolType ladder = new ToolType("Ladder", 1.99, true, true, false);
-        ToolType chainsaw = new ToolType("Chainsaw", 1.49, true, false, true);
-        ToolType jackhammer = new ToolType("Jackhammer", 2.99, true, false, false);
+    ToolType chainsaw =
+        new ToolType(
+            "Chainsaw",
+            1.49,
+            new HashSet<DayType>() {
+              {
+                add(WEEKDAY);
+                add(HOLIDAY);
+              }
+            });
 
-        tools.add(new Tool("CHNS", chainsaw, "Stihl"));
-        tools.add(new Tool("LADW", ladder, "Werner"));
-        tools.add(new Tool("JAKD", jackhammer, "DeWalt"));
-        tools.add(new Tool("JAKR", jackhammer, "Ridgid"));
-    }
+    ToolType jackhammer =
+        new ToolType(
+            "Jackhammer",
+            2.99,
+            new HashSet<DayType>() {
+              {
+                add(WEEKDAY);
+              }
+            });
 
-    public Tool findTool(String toolCode) {
-        return tools.stream()
-                .filter(tool -> tool.getToolCode().equals(toolCode))
-                .findFirst()
-                .orElse(null);
-    }
+    tools.add(new Tool("CHNS", chainsaw, "Stihl"));
+    tools.add(new Tool("LADW", ladder, "Werner"));
+    tools.add(new Tool("JAKD", jackhammer, "DeWalt"));
+    tools.add(new Tool("JAKR", jackhammer, "Ridgid"));
+  }
+
+  public Tool findTool(String toolCode) {
+    return tools.stream()
+        .filter(tool -> tool.getToolCode().equals(toolCode))
+        .findFirst()
+        .orElse(null);
+  }
 }
