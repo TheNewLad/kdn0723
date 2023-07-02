104330a6b4dbd67ba4c3dc495e3ae0e7381e110d
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2023-07-02 00:27:51 -0500

    Refactor DayChecker

--- src/util/DayChecker.java
+++ src/util/DayChecker.java
@@ -9,37 +9,47 @@ import java.util.List;
 
 public class DayChecker {
 
-    public static boolean isWeekend(LocalDate date) {
-        return date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
+  public static DayType getDayType(LocalDate date) {
+    if (isHoliday(date)) {
+      return DayType.HOLIDAY;
+    }
+    return isWeekday(date) ? DayType.WEEKDAY : DayType.WEEKEND;
+  }
+
+  private static boolean isWeekend(LocalDate date) {
+    return date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
+  }
+
+  private static boolean isWeekday(LocalDate date) {
+    return !isWeekend(date);
+  }
+
+  private static boolean isHoliday(LocalDate date) {
+    LocalDate laborDay = getLaborDay(date.getYear());
+    LocalDate independenceDay = getObservableHoliday(LocalDate.of(date.getYear(), 7, 4));
+
+    List<LocalDate> holidays = Arrays.asList(laborDay, independenceDay);
+
+    return holidays.contains(date);
+  }
+
+  private static LocalDate getObservableHoliday(LocalDate date) {
+    if (date.getDayOfWeek() == DayOfWeek.SATURDAY) {
+      return date.minusDays(1);
+    } else if (date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+      return date.plusDays(1);
     }
 
-    public static boolean isWeekday(LocalDate date) {
-        return !isWeekend(date);
-    }
+    return date;
+  }
 
-    public static boolean isHoliday(LocalDate date) {
-        LocalDate laborDay = getLaborDay(date.getYear());
-        LocalDate independenceDay = getObservableHoliday(LocalDate.of(date.getYear(), 7, 4));
+  private static LocalDate getLaborDay(int year) {
+    return LocalDate.of(year, 9, 1).with(firstInMonth(DayOfWeek.MONDAY));
+  }
 
-        List<LocalDate> holidays = Arrays.asList(
-                laborDay,
-                independenceDay
-        );
-
-        return holidays.contains(date);
-    }
-
-    private static LocalDate getObservableHoliday(LocalDate date) {
-            if (date.getDayOfWeek() == DayOfWeek.SATURDAY) {
-                return date.minusDays(1);
-            } else if (date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-                return date.plusDays(1);
-            }
-
-        return date;
-    }
-
-    private static LocalDate getLaborDay(int year) {
-        return LocalDate.of(year, 9, 1).with(firstInMonth(DayOfWeek.MONDAY));
-    }
+  public enum DayType {
+    WEEKDAY,
+    WEEKEND,
+    HOLIDAY
+  }
 }
--- test/util/DayCheckerTest.java
+++ test/util/DayCheckerTest.java
@@ -1,66 +1,52 @@
 package util;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static util.DayChecker.DayType.*;
+import static util.DayChecker.getDayType;
 
 import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
 class DayCheckerTest {
-    @Test
-    public void Should_ReturnFalse_When_DateIsNotAHoliday() {
-        LocalDate date = LocalDate.of(2023, 2, 10);
-        assertFalse(DayChecker.isHoliday(date));
-    }
+  @Test
+  public void Should_ReturnWeekday_When_WeekdayDateIsNotAHoliday() {
+    LocalDate date = LocalDate.of(2023, 2, 10);
+    assertEquals(WEEKDAY, getDayType(date));
+  }
 
-    @Test
-    public void Should_ReturnTrue_When_WeekdayDateIsIndependenceDay() {
-        LocalDate date = LocalDate.of(2023, 7, 4);
-        assertTrue(DayChecker.isHoliday(date));
-    }
+  @Test
+  public void Should_ReturnWeekend_When_WeekendDateIsNotAHoliday() {
+    LocalDate date = LocalDate.of(2023, 2, 11);
+    assertEquals(WEEKEND, getDayType(date));
+  }
 
-    @Test
-    public void Should_ReturnFalse_When_WeekendDateIsIndependenceDay() {
-        LocalDate saturdayIndependenceDay = LocalDate.of(2020, 7, 4);
-        LocalDate sundayIndependenceDay = LocalDate.of(2021, 7, 4);
-        assertAll(
-                () -> assertFalse(DayChecker.isHoliday(saturdayIndependenceDay)),
-                () -> assertFalse(DayChecker.isHoliday(sundayIndependenceDay))
-        );
-    }
+  @Test
+  public void Should_ReturnHoliday_When_WeekdayDateIsIndependenceDay() {
+    LocalDate date = LocalDate.of(2023, 7, 4);
+    assertEquals(HOLIDAY, getDayType(date));
+  }
 
-    @Test
-    public void Should_ReturnTrue_When_WeekdayDateIsIndependenceDayObserved() {
-        LocalDate saturdayIndependenceDay = LocalDate.of(2020, 7, 3);
-        LocalDate sundayIndependenceDay = LocalDate.of(2021, 7, 5);
-        assertAll(
-                () -> assertTrue(DayChecker.isHoliday(saturdayIndependenceDay)),
-                () -> assertTrue(DayChecker.isHoliday(sundayIndependenceDay))
-        );
-    }
+  @Test
+  public void Should_ReturnWeekend_When_WeekendDateIsIndependenceDay() {
+    LocalDate saturdayIndependenceDay = LocalDate.of(2020, 7, 4);
+    LocalDate sundayIndependenceDay = LocalDate.of(2021, 7, 4);
+    assertAll(
+        () -> assertEquals(WEEKEND, getDayType(saturdayIndependenceDay)),
+        () -> assertEquals(WEEKEND, getDayType(sundayIndependenceDay)));
+  }
 
-    @Test
-    public void Should_ReturnTrue_When_DateIsLaborDay() {
-        LocalDate date = LocalDate.of(2023, 9, 4);
-        assertTrue(DayChecker.isHoliday(date));
-    }
+  @Test
+  public void Should_ReturnHoliday_When_WeekdayDateIsIndependenceDayObserved() {
+    LocalDate fridayIndependenceDayObserved = LocalDate.of(2020, 7, 3);
+    LocalDate mondayIndependenceDayObserved = LocalDate.of(2021, 7, 5);
+    assertAll(
+            () -> assertEquals(HOLIDAY, getDayType(fridayIndependenceDayObserved)),
+            () -> assertEquals(HOLIDAY, getDayType(mondayIndependenceDayObserved)));
+  }
 
-    @Test
-    public void Should_ReturnTrue_When_DateIsWeekend() {
-        LocalDate saturday = LocalDate.of(2023, 7, 1);
-        LocalDate sunday = LocalDate.of(2023, 7, 2);
-        assertAll(
-                () -> assertTrue(DayChecker.isWeekend(saturday)),
-                () -> assertTrue(DayChecker.isWeekend(sunday))
-        );
-    }
-
-    @Test
-    public void Should_ReturnTrue_When_DateIsWeekday() {
-        LocalDate friday = LocalDate.of(2023, 6, 30);
-        LocalDate monday = LocalDate.of(2023, 7, 3);
-        assertAll(
-                () -> assertTrue(DayChecker.isWeekday(friday)),
-                () -> assertTrue(DayChecker.isWeekday(monday))
-        );
-    }
+  @Test
+  public void Should_ReturnHoliday_When_DateIsLaborDay() {
+    LocalDate date = LocalDate.of(2023, 9, 4);
+    assertTrue(HOLIDAY == getDayType(date));
+  }
 }
