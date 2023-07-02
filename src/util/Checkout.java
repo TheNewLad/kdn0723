e421200b7d6b2bb4214d228d89e7aa5598e18148
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2023-07-02 01:01:48 -0500

    Display number of charge days on rental agreement

--- src/util/Checkout.java
+++ src/util/Checkout.java
@@ -1,11 +1,13 @@
 package util;
 
+import static util.DayChecker.*;
 import static util.Formatter.formatCurrency;
 import static util.Formatter.formatDate;
 
 import datasource.ToolStore;
 import java.time.LocalDate;
 import model.Tool;
+import model.ToolType;
 
 public class Checkout {
 
@@ -23,7 +25,7 @@ public class Checkout {
         ToolStore.getInstance()
             .findTool(
                 toolCode); // I assume that the tool code is valid and exists in the database at
-                           // this point.
+    // this point.
     this.rentalDayCount = rentalDayCount;
     this.discountPercent = discountPercent;
     this.checkoutDate = checkoutDate;
@@ -45,21 +47,56 @@ public class Checkout {
     return checkoutDate;
   }
 
-    public LocalDate getDueDate() {
-        return checkoutDate.plusDays(rentalDayCount);
+  public LocalDate getDueDate() {
+    return checkoutDate.plusDays(rentalDayCount);
+  }
+
+  private int getChargeDays() {
+    int chargeDays = 0;
+    LocalDate date = checkoutDate.plusDays(1); // Add 1 to exclude the checkout date.
+    LocalDate dueDate = getDueDate().plusDays(1); // Add 1 to include the due date.
+
+    while (date.isBefore(dueDate)) {
+      DayType dayType = getDayType(date);
+      ToolType toolType = getTool().getToolType();
+
+      if (shouldChargeForDayType(dayType, toolType)) {
+        chargeDays++;
+      }
+
+      date = date.plusDays(1);
     }
 
+    return chargeDays;
+  }
+
+  private boolean shouldChargeForDayType(DayType dayType, ToolType toolType) {
+    switch (dayType) {
+      case HOLIDAY:
+        return toolType.isHolidayCharge();
+      case WEEKEND:
+        return toolType.isWeekendCharge();
+      case WEEKDAY:
+        return toolType.isWeekdayCharge();
+      default:
+        return false;
+    }
+  }
+
   public void generateRentalAgreement() {
+    Tool tool = getTool();
+    ToolType toolType = tool.getToolType();
+
     RentalAgreement rentalAgreement =
         new RentalAgreement(
-            getTool().getToolCode(),
-            getTool().getToolType().getType(),
-            getTool().getBrand(),
+            tool.getToolCode(),
+            toolType.getType(),
+            tool.getBrand(),
             String.valueOf(getRentalDayCount()),
             formatDate(getCheckoutDate()),
             formatDate(getDueDate()),
-            formatCurrency(getTool().getToolType().getDailyCharge()),
-            "",
+            formatCurrency(toolType.getDailyCharge()),
+            String.valueOf(getChargeDays()),
             "",
             "",
             "",
--- test/util/CheckoutTest.java
+++ test/util/CheckoutTest.java
@@ -12,121 +12,169 @@ import org.junit.jupiter.api.Test;
 
 class CheckoutTest {
 
-    private static final PrintStream systemOut = System.out;
-    private static final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private static final PrintStream testOut = new PrintStream(outputStream);
+  private static final PrintStream systemOut = System.out;
+  private static final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+  private static final PrintStream testOut = new PrintStream(outputStream);
 
-    @BeforeAll
-    public static void setUp() {
-        System.setOut(testOut);
-    }
+  @BeforeAll
+  public static void setUp() {
+    System.setOut(testOut);
+  }
 
-    @AfterAll
-    public static void tearDown() {
-        System.setOut(systemOut);
-    }
+  @AfterAll
+  public static void tearDown() {
+    System.setOut(systemOut);
+  }
 
-    @Test
-    public void Should_ThrowException_When_RentalDayCountIsLessThanOne() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            Checkout checkout = new Checkout("LADW", 0, 1, LocalDate.of(2023, 7, 3));
+  @Test
+  public void Should_ThrowException_When_RentalDayCountIsLessThanOne() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> {
+          Checkout checkout = new Checkout("LADW", 0, 1, LocalDate.of(2023, 7, 3));
         });
-    }
+  }
 
-    @Test
-    public void Should_NotThrowException_When_RentalDayCountIsOneOrGreater() {
-        Checkout checkout1 = new Checkout("LADW", 1, 1, LocalDate.of(2023, 7, 3));
-        Checkout checkout2 = new Checkout("LADW", 1000, 1, LocalDate.of(2023, 7, 3));
-    }
+  @Test
+  public void Should_NotThrowException_When_RentalDayCountIsOneOrGreater() {
+    Checkout checkout1 = new Checkout("LADW", 1, 1, LocalDate.of(2023, 7, 3));
+    Checkout checkout2 = new Checkout("LADW", 1000, 1, LocalDate.of(2023, 7, 3));
+  }
 
-    @Test
-    public void Should_ThrowException_When_DiscountPercentageIsOutOfBounds() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            Checkout checkout1 = new Checkout("LADW", 1, -1, LocalDate.of(2023, 7, 3));
-            Checkout checkout2 = new Checkout("LADW", 1, 101, LocalDate.of(2023, 7, 3));
+  @Test
+  public void Should_ThrowException_When_DiscountPercentageIsOutOfBounds() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> {
+          Checkout checkout1 = new Checkout("LADW", 1, -1, LocalDate.of(2023, 7, 3));
+          Checkout checkout2 = new Checkout("LADW", 1, 101, LocalDate.of(2023, 7, 3));
         });
-    }
+  }
 
-    @Test
-    public void Should_NotThrowException_When_DiscountPercentageIsInBounds() {
-        Checkout checkout1 = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        Checkout checkout2 = new Checkout("LADW", 1, 100, LocalDate.of(2023, 7, 3));
-    }
+  @Test
+  public void Should_NotThrowException_When_DiscountPercentageIsInBounds() {
+    Checkout checkout1 = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    Checkout checkout2 = new Checkout("LADW", 1, 100, LocalDate.of(2023, 7, 3));
+  }
 
-    @Test
-    public void RentalAgreement_Should_ContainToolCode_OnCheckout() {
-        Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        checkout.generateRentalAgreement();
+  @Test
+  public void RentalAgreement_Should_ContainToolCode_OnCheckout() {
+    Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    checkout.generateRentalAgreement();
 
-        String consoleOutput = outputStream.toString();
-        String expectedOutput = "Tool code: LADW";
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Tool code: LADW";
 
-        assertTrue(consoleOutput.contains(expectedOutput));
-    }
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
 
-    @Test
-    public void RentalAgreement_Should_ContainToolType_OnCheckout() {
-        Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        checkout.generateRentalAgreement();
+  @Test
+  public void RentalAgreement_Should_ContainToolType_OnCheckout() {
+    Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    checkout.generateRentalAgreement();
 
-        String consoleOutput = outputStream.toString();
-        String expectedOutput = "Tool type: Ladder";
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Tool type: Ladder";
 
-        assertTrue(consoleOutput.contains(expectedOutput));
-    }
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
 
-    @Test
-    public void RentalAgreement_Should_ContainToolBrand_OnCheckout() {
-        Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        checkout.generateRentalAgreement();
+  @Test
+  public void RentalAgreement_Should_ContainToolBrand_OnCheckout() {
+    Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    checkout.generateRentalAgreement();
 
-        String consoleOutput = outputStream.toString();
-        String expectedOutput = "Tool brand: Werner";
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Tool brand: Werner";
 
-        assertTrue(consoleOutput.contains(expectedOutput));
-    }
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
 
-    @Test
-    public void RentalAgreement_Should_ContainRentalDays_OnCheckout() {
-        Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        checkout.generateRentalAgreement();
+  @Test
+  public void RentalAgreement_Should_ContainRentalDays_OnCheckout() {
+    Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    checkout.generateRentalAgreement();
 
-        String consoleOutput = outputStream.toString();
-        String expectedOutput = "Rental days: 1";
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Rental days: 1";
 
-        assertTrue(consoleOutput.contains(expectedOutput));
-    }
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
 
-    @Test
-    public void RentalAgreement_Should_ContainCheckoutDate_OnCheckout() {
-        Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        checkout.generateRentalAgreement();
+  @Test
+  public void RentalAgreement_Should_ContainCheckoutDate_OnCheckout() {
+    Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    checkout.generateRentalAgreement();
 
-        String consoleOutput = outputStream.toString();
-        String expectedOutput = "Check out date: 07/03/23";
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Check out date: 07/03/23";
 
-        assertTrue(consoleOutput.contains(expectedOutput));
-    }
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
 
-    @Test
-    public void RentalAgreement_Should_HaveCalculatedDueDate_OnCheckout() {
-        Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        checkout.generateRentalAgreement();
+  @Test
+  public void RentalAgreement_Should_HaveCalculatedDueDate_OnCheckout() {
+    Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    checkout.generateRentalAgreement();
 
-        String consoleOutput = outputStream.toString();
-        String expectedOutput = "Due date: 07/04/23";
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Due date: 07/04/23";
 
-        assertTrue(consoleOutput.contains(expectedOutput));
-    }
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
 
-    @Test
-    public void RentalAgreement_Should_ContainDailyRentalCharge_OnCheckout() {
-        Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
-        checkout.generateRentalAgreement();
+  @Test
+  public void RentalAgreement_Should_ContainDailyRentalCharge_OnCheckout() {
+    Checkout checkout = new Checkout("LADW", 1, 0, LocalDate.of(2023, 7, 3));
+    checkout.generateRentalAgreement();
 
-        String consoleOutput = outputStream.toString();
-        String expectedOutput = "Daily rental charge: $1.99";
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Daily rental charge: $1.99";
 
-        assertTrue(consoleOutput.contains(expectedOutput));
-    }
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
+
+  @Test
+  public void RentalAgreement_Should_ContainCorrectChargeDays_When_LadderIsRented() {
+    Checkout checkout = new Checkout("LADW", 5, 0, LocalDate.of(2023, 6, 30));
+    checkout.generateRentalAgreement();
+
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Charge days: 4";
+
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
+
+  @Test
+  public void RentalAgreement_Should_ContainCorrectChargeDays_When_ChainsawIsRented() {
+    Checkout checkout = new Checkout("CHNS", 5, 0, LocalDate.of(2023, 6, 30));
+    checkout.generateRentalAgreement();
+
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Charge days: 3";
+
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
+
+  @Test
+  public void RentalAgreement_Should_ContainCorrectChargeDays_When_DewaltJackhammerIsRented() {
+    Checkout checkout = new Checkout("JAKD", 5, 0, LocalDate.of(2023, 6, 30));
+    checkout.generateRentalAgreement();
+
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Charge days: 2";
+
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
+
+  @Test
+  public void RentalAgreement_Should_ContainCorrectChargeDays_When_RigidJackhammerIsRented() {
+    Checkout checkout = new Checkout("JAKR", 5, 0, LocalDate.of(2023, 6, 30));
+    checkout.generateRentalAgreement();
+
+    String consoleOutput = outputStream.toString();
+    String expectedOutput = "Charge days: 2";
+
+    assertTrue(consoleOutput.contains(expectedOutput));
+  }
 }
